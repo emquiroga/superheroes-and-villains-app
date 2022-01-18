1eeae2dfa7f0cdbef69d6b83e9ccfe955945f27a
Emiliano <emiliano.quiroga093@gmail.com>
2022-01-18 12:27:58 -0300

    creating redux branch

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useState } from "react";
 import { useFormik } from "formik";
 import axios from "axios";
 
-// import { login } from "../../services/login";
 import { AuthContext } from "../../contexts/AuthContext";
 import { useHistory } from "react-router";
 import { authActions } from "../../actions/authActions";
